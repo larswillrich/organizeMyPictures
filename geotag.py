c8281724085394a574750da2845a807e1aab513d
Lars Willrich <lars.willrich@share-now.com>
2020-04-06 00:01:49 +0200

    upload initial state with test data

---
+++ geotag.py
@@ -0,0 +1,99 @@
+# ==========================================
+# ==========================================
+# 
+# This source code is based on 
+# https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3
+# 
+# ==========================================
+# ==========================================
+
+
+import requests
+from PIL.ExifTags import GPSTAGS, TAGS
+from PIL import Image
+
+def get_exif(filename):
+    image = Image.open(filename)
+    image.verify()
+
+    try:
+        return image._getexif()
+    except AttributeError:
+        return None
+
+def get_geotagging(exif):
+    if not exif:
+        return None
+        #raise ValueError("No EXIF metadata found")
+
+    geotagging = {}
+    for (idx, tag) in TAGS.items():
+        if tag == 'GPSInfo':
+            if idx not in exif:
+                return None
+                #raise ValueError("No EXIF geotagging found")
+
+            for (key, val) in GPSTAGS.items():
+                if key in exif[idx]:
+                    geotagging[val] = exif[idx][key]
+
+    return geotagging
+
+def get_decimal_from_dms(dms, ref):
+
+    degrees = dms[0][0] / dms[0][1]
+    minutes = dms[1][0] / dms[1][1] / 60.0
+    seconds = dms[2][0] / dms[2][1] / 3600.0
+
+    if ref in ['S', 'W']:
+        degrees = -degrees
+        minutes = -minutes
+        seconds = -seconds
+
+    return round(degrees + minutes + seconds, 5)
+
+def get_coordinates(geotags):
+    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+
+    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+
+    return (lat,lon)
+
+def get_location(geotags):
+    coords = get_coordinates(geotags)
+
+    uri = 'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json'
+    headers = {}
+    params = {
+        'app_id': '',
+        'app_code': '',
+        'prox': "%s,%s" % coords,
+        'gen': 9,
+        'mode': 'retrieveAddresses',
+        'maxresults': 1,
+    }
+
+    response = requests.get(uri, headers=headers, params=params)
+    try:
+        response.raise_for_status()
+        return response.json()
+
+    except requests.exceptions.HTTPError as e:
+        print(str(e))
+        return {}
+
+
+def getGeoData(file):
+    exif = get_exif(file)
+
+    geotags = None if exif == None else get_geotagging(exif)
+    
+    if (geotags == None): return None, None, exif
+
+    if 'GPSLatitude' not in geotags: return None, geotags, exif
+
+#    uncommended, because I don't want to speak with the HERE API in testing due to ressource limits in free version
+#    include again!!
+#
+#    return get_location(geotags)['Response']['View'][0]['Result'][0]['Location']['Address']['Label'], geotags, exif
+    return 'GEO_LOCATION', geotags, exif
---
+++ order.py
@@ -0,0 +1,107 @@
+
+class Pictures:
+    import os
+    import hashlib
+    import json
+    import geotag
+    from os.path import join, getsize
+    from datetime import datetime
+    import base64
+    import pandas as pd
+
+    # this is the used key in EXIF dictinary for geo location data
+    GEOKEY = 34853
+
+    # analised files, adapt or extend it
+    FILE_EXTENSIONS = ['.jpg', '.png']
+
+    def __init__(self, path):
+        self.path = path
+        self.resetCounter()
+
+    prettyprint = lambda self, obj: print(self.json.dumps(obj, indent=4, ensure_ascii=False))
+
+    def incrementCounter(self):
+        self.COUNTER += 1
+        return self.COUNTER
+
+    def resetCounter(self):
+        self.COUNTER = 0
+
+    def fileCriteriaAreGiven(self, currentFile):
+        if self.incrementCounter() % 100 == 0:
+            print('{} '.format(self.COUNTER), end='', flush=True)
+        iteration = iter(i for i in Pictures.FILE_EXTENSIONS if currentFile.lower().endswith(i))
+        return next(iteration, None) != None
+
+    def getMd5(self, fname):
+        hash_md5 = self.hashlib.md5()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
+
+    getmtime = lambda self, file: self.datetime.utcfromtimestamp(self.os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
+    getctime = lambda self, file: self.datetime.utcfromtimestamp(self.os.path.getctime(file)).strftime('%Y-%m-%d %H:%M:%S')
+
+    def collect(self):
+        # get all files
+        print('collect files in ...{}'.format(self.path))
+        getAllFiles = lambda self: [self.os.path.join(root, file) for root, dir, files in self.os.walk(self.path, topdown=False) for file in files]
+
+        allFiles = getAllFiles(self)
+        print('{} files found'.format(len(allFiles)))
+
+        # organize in dictinary with usefull informations and filter out all 'non pictures'
+
+        print('Start collecting metainformation')
+        self.pictureDict = [
+            {'path': file, 
+            'hash': self.getMd5(file), 
+            'creationTime': self.getctime(file),
+            'modificationTime': self.getmtime(file),
+            'size': self.os.path.getsize(file)
+            } 
+            for file in allFiles if self.fileCriteriaAreGiven(file)]
+
+        print('Start collecting GEO Information')
+        self.resetCounter()
+
+        for file in self.pictureDict:
+            if self.incrementCounter() % 100 == 0:
+                print('{} '.format(self.COUNTER), end='', flush=True)
+
+            location, geotags, exif = self.geotag.getGeoData(file['path'])
+            
+            file['location'] = location
+            if geotags != None:
+                for i in geotags.keys():
+                    file[i] = geotags[i]
+                    
+                if self.GEOKEY in exif:
+                    file['geoTagBase64'] = self.base64.b64encode(str(exif[Pictures.GEOKEY]).encode('ascii')) 
+
+    def saveDF(self, csvPath):
+        picDF = self.pd.DataFrame(self.pictureDict)
+        picDF.to_csv(csvPath, index=False, header=True)
+
+    def printDublicates(self):
+        duplicates = {}
+        for picture in self.pictureDict:
+            if picture['hash'] in duplicates:
+                duplicates[picture['hash']] = duplicates[picture['hash']] + 1
+            else:
+                duplicates[picture['hash']] = 0
+
+        self.prettyprint([{"{}".format(k): "{}".format(v)} for k, v in duplicates.items() if v > 0])
+            
+
+# this path is the path to your pictures, most likely you want to adapt this!
+path = '/Volumes/DATA/Lars_Data/media/Media/Handy/'
+#path = '/Volumes/DATA/Test'
+
+pictures = Pictures(path)
+pictures.collect()
+pictures.saveDF('files.csv')
+
+pictures.printDublicates()
